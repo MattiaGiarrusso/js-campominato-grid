400516e2d4cd1b2c9e7fd386cd1c793b6cd266af
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-03-15 21:20:10 +0100

    Feat: add comment in js

--- js/script.js
+++ js/script.js
@@ -7,20 +7,27 @@
 const mainGrid = document.querySelector('#grid');
 console.log(mainGrid);
 
+// Creo il ciclo for per il conteggio di 100 celle numerate da 1 a 100
 for(let i=1; i<101; i++) {
+    // Dichiaro la variabile che richiamerà la funzione che genera i 100 numeri
     const square = generateSquare(i);
     console.log(square);
+    // Appendo i quadrati alla griglia in html
     mainGrid.append(square);
 }
 
+// Creo la funzione che genera numeri
 function generateSquare(number) {
+    // dichiaro la variabile per creare l'elemento square
     const newSquare = document.createElement('div');
     newSquare.classList.add('square');
     newSquare.innerHTML = `<span>${number}</span>`;
-    
+
+    // assegno alla variabile 'newSquare' una funzione al click
     newSquare.addEventListener('click', function(){
         console.log(this.children[0].innerHTML);
         newSquare.classList.toggle('click-cell');
     });
+    // ritorno il risultato della funzione
     return newSquare;
 }
